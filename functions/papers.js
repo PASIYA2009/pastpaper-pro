39b47c784f91e004e5f42e6fceed9590993c53d4
PASIYA2009 <eshandinujaya86@gmail.com>
2026-09-11 12:49:01 +0530

    Delete functions directory

--- functions/papers.js
+++
@@ -1,1 +0,0 @@
-const gh=require('./_github');exports.handler=async()=>{try{return {statusCode:200,headers:{'Content-Type':'application/json','Cache-Control':'public,max-age=60'},body:JSON.stringify(await gh.list())}}catch(e){return {statusCode:500,body:JSON.stringify({error:e.message})}}};
