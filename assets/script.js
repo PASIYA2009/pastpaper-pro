6d406d30f4de48b6dc15c1234d63e7763bf38987
PASIYA2009 <eshandinujaya86@gmail.com>
2026-09-11 12:48:37 +0530

    Delete assets directory

--- assets/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('[data-confirm]').forEach((el) => {
-        el.addEventListener('click', (event) => {
-            if (!confirm(el.dataset.confirm)) event.preventDefault();
-        });
-    });
-});
